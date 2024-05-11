7068be77cbe9eeac6c5ca54cad19b572975995dd
Mohd Aadil Rana <aadilranajmi@gmail.com>
2024-05-11 20:51:00 +0530

    job list and create job UI fixed

--- src/components/jobs/JobsList.js
+++ src/components/jobs/JobsList.js
@@ -49,7 +49,7 @@ const JobsList = () => {
   const currentJobs = jobs?.slice(indexOfFirstJob, indexOfLastJob) || [];
 
   // Change page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber); 
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   // Calculate page numbers
   const pageNumbers = [];
@@ -79,12 +79,36 @@ const JobsList = () => {
             </div>
           </div>
 
-          <input
-            type="text"
-            placeholder="Search Jobs"
-            value={searchQuery}
-            onChange={handleSearchInputChange}
-          />
+          <div className="my-4">
+            <label htmlFor="table-search" className="sr-only">
+              Search
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                type="text"
+                id="table-search-users"
+                className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search for users"
+              />
+            </div>
+          </div>
 
           <div className="mb-4 border-gray-200"></div>
           <div id="default-styled-tab-content">
@@ -236,13 +260,13 @@ const JobsList = () => {
                 </tbody>
 
                 {/* Pagination */}
-                <ul className="flex">
+                <ul className="flex my-2 ml-3">
                   {pageNumbers.map((number) => (
                     <li key={number} className="mr-2">
                       <a
                         onClick={() => paginate(number)}
                         href="#"
-                        className="px-3 py-1 rounded-md bg-gray-200 hover:bg-gray-300"
+                        className="px-3 py-1 rounded-md bg-gray-200 hover:bg-gray-300 inline-block"
                       >
                         {number}
                       </a>
