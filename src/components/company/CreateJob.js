c60a243c6e21584eb2972e53646cd2bb6d2f558c
Mohd Aadil Rana <aadilranajmi@gmail.com>
2024-05-13 10:19:42 +0530

    create job UI improved

--- src/components/company/CreateJob.js
+++ src/components/company/CreateJob.js
@@ -200,16 +200,17 @@ const CreateJob = () => {
             </div>
           </div>
 
-          <section className="bg-white mt-5 pt-5 py-8">
-            <Formik
-              initialValues={initialValues}
-              validationSchema={validationSchema}
-              onSubmit={handleSubmit}
-            >
-              {({ isSubmitting }) => (
-                <Form id="companyProfileForm" encType="multipart/form-data">
-                  <div className="flex px-5 justify-between mb-4">
-                    <div className="ps-3 gap-x-8 justify-around font-poppins flex-wrap grid grid-cols-3">
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ isSubmitting }) => (
+              <Form id="companyProfileForm" encType="multipart/form-data">
+                <section className="bg-white mt-5 pt-0 py-8">
+                  <h3 className="text-base p-3 border-b border-gray-200 mb-4">Job Details</h3>
+                  <div className="flex px-5 justify-between mb-4 w-full">
+                    <div className="ps-3 gap-x-8 justify-around font-poppins flex-wrap grid grid-cols-3 w-full">
                       <Field name="job_title">
                         {({ field }) => (
                           <div className="mb-3">
@@ -385,29 +386,210 @@ const CreateJob = () => {
                       </div>
                     </div>
                   </div>
+                </section>
 
-                  <div className="flex px-10 font-poppins pt-3 justify-between">
-                    <div></div>
-                    <div className="flex gap-4">
-                      <button
-                        type="button"
-                        className="px-6 py-2 text-base rounded font-normal bg-F4F4F4 focus:outline-none"
-                      >
-                        CANCEL
-                      </button>
-                      <button
-                        type="submit"
-                        className="px-6 py-2 text-base rounded font-normal bg-1D4469 text-white focus:outline-none"
-                        disabled={isSubmitting}
-                      >
-                        {isSubmitting ? "SAVING..." : "SAVE"}
-                      </button>
+                <section className="bg-white mt-5 pt-0 py-8">
+                  <h3 className="text-base p-3 border-b border-gray-200 mb-4">Job Details</h3>
+                  <div className="flex px-5 justify-between mb-4 w-full">
+                    <div className="ps-3 gap-x-8 justify-around font-poppins flex-wrap grid grid-cols-3 w-full">
+                      <Field name="job_title">
+                        {({ field }) => (
+                          <div className="mb-3">
+                            <label
+                              htmlFor="job_title"
+                              className="block mb-2 text-535252 text-16 font-400 text-535252"
+                            >
+                              Job Title
+                            </label>
+                            <input
+                              {...field}
+                              className="inputBorder text-sm block w-full p-2.5 italic"
+                              type="text"
+                              placeholder="Enter title..."
+                              onKeyUp={handleKeyUp}
+                            />
+                            <ErrorMessage
+                              name="job_title"
+                              component="div"
+                              className="text-red-500 text-sm"
+                            />
+                          </div>
+                        )}
+                      </Field>
+
+                      <Field name="Select Company *">
+                        {({ field }) => (
+                          <div>
+                            <label
+                              htmlFor="display_name"
+                              className="block mb-2 text-535252 text-16  font-400"
+                            >
+                              Select Company *
+                            </label>
+
+                            <Field
+                              as="select"
+                              id="business_category"
+                              name="business_category"
+                              className="form-select border border-gray-300 text-gray-900 text-sm block w-full px-2.5"
+                            >
+                              <option value="">Select category</option>
+                              <option value="1">Group 1</option>
+                              <option value="2">Group 2</option>
+                              <option value="3">Group 3</option>
+                              <option value="4">Group 4</option>
+                              <option value="5">Group 5</option>
+                            </Field>
+
+                            <ErrorMessage
+                              name="select_company"
+                              component="div"
+                              className="text-red-500 text-sm"
+                            />
+                          </div>
+                        )}
+                      </Field>
+
+                      <Field name="Category">
+                        {({ field }) => (
+                          <div>
+                            <label
+                              htmlFor="Category"
+                              className="block mb-2  text-535252 text-16  font-400 "
+                            >
+                              Category *
+                            </label>
+
+                            <Field
+                              as="select"
+                              id="category"
+                              name="category"
+                              className="form-select border border-gray-300 text-gray-900 text-sm block w-full px-2.5"
+                            >
+                              <option value="">Select category</option>
+                              <option value="1">Group 1</option>
+                              <option value="2">Group 2</option>
+                              <option value="3">Group 3</option>
+                              <option value="4">Group 4</option>
+                              <option value="5">Group 5</option>
+                            </Field>
+
+                            <ErrorMessage
+                              name="category"
+                              component="div"
+                              className="text-red-500 text-sm"
+                            />
+                          </div>
+                        )}
+                      </Field>
+
+                      <Field name="type">
+                        {({ field }) => (
+                          <div>
+                            <div className="mb-2">
+                              <label
+                                htmlFor="types"
+                                className="mb-1 text-535252 text-16 font-400"
+                              >
+                                Total Vacancies *
+                              </label>
+                            </div>
+
+                            <input
+                              {...field}
+                              className="inputBorder text-sm block w-full p-2.5 italic"
+                              type="text"
+                              placeholder="Enter total_vacancies..."
+                              onKeyUp={handleKeyUp}
+                            />
+                            <ErrorMessage
+                              name="total_vacancies"
+                              component="div"
+                              className="text-red-500 text-sm"
+                            />
+                          </div>
+                        )}
+                      </Field>
+
+                      <Field name="Deadline">
+                        {({ field }) => (
+                          <div>
+                            <div className="pe-2 flex self-end w-full text-center">
+                              <label
+                                htmlFor="Deadline *"
+                                className="mb-1 text-535252 text-16 font-400"
+                              >
+                                Deadline *
+                              </label>
+                            </div>
+
+                            <input
+                              {...field}
+                              className="flex items-center p-1 w-full text-center text-gray-500"
+                              type="text"
+                              placeholder="Enter total_vacancies..."
+                              onKeyUp={handleKeyUp}
+                            />
+                            <ErrorMessage
+                              name="total_vacancies"
+                              component="div"
+                              className="text-red-500 text-sm"
+                            />
+                          </div>
+                        )}
+                      </Field>
+
+                      <div className="">
+                        <label
+                          htmlFor="business_category"
+                          className="block mb-1 text-535252 text-16 font-400"
+                        >
+                          Business Category
+                        </label>
+                        <Field
+                          as="select"
+                          id="business_category"
+                          name="business_category"
+                          className="form-select border border-gray-300 text-gray-900 text-sm block w-full px-2.5"
+                        >
+                          <option value="">Select category</option>
+                          <option value="1">Group 1</option>
+                          <option value="2">Group 2</option>
+                          <option value="3">Group 3</option>
+                          <option value="4">Group 4</option>
+                          <option value="5">Group 5</option>
+                        </Field>
+                        <ErrorMessage
+                          name="business_category"
+                          component="div"
+                          className="text-red-500 text-sm"
+                        />
+                      </div>
                     </div>
                   </div>
-                </Form>
-              )}
-            </Formik>
-          </section>
+                </section>
+
+                <div className="flex px-10 font-poppins pt-3 justify-between">
+                  <div></div>
+                  <div className="flex gap-4">
+                    <button
+                      type="button"
+                      className="px-6 py-2 text-base rounded font-normal bg-F4F4F4 focus:outline-none"
+                    >
+                      CANCEL
+                    </button>
+                    <button
+                      type="submit"
+                      className="px-6 py-2 text-base rounded font-normal bg-1D4469 text-white focus:outline-none"
+                      disabled={isSubmitting}
+                    >
+                      {isSubmitting ? "SAVING..." : "SAVE"}
+                    </button>
+                  </div>
+                </div>
+              </Form>
+            )}
+          </Formik>
 
           {/*
       <CompanyProfile />
