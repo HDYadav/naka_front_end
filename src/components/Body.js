914f399c3dd27c2f9ea307596c20dd5bf3f45c0a
Hari Yadav <h.hariy2k@gmail.com>
2024-05-19 10:15:43 +0530

    dummy url removed

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,16 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login'; 
 import Dashboard from './Dashboard'; 
-import Customer from './customer/Customer'; 
-import ProtectedRoute from '../utils/ProtectedRoute';
-import AddCustomer from './customer/AddCustomer';
-import AddCompany from './company/AddCompany';
-import AddCustomer1 from './customer/AddCustomer1';
-import Importcha from './chartofaccount/Importcha';
-import AssetList from './chartofaccount/AssetList';
-import EditCompany from './company/EditCompany'; 
-import SignupForm from './form/SignupForm';
-import AddVendor from './company/AddVendor';
+import ProtectedRoute from "../utils/ProtectedRoute";
+import SignupForm from "./form/SignupForm";
 import JobsList from './jobs/JobsList';
 import JobDetails from "./jobs/JobDetails";
 import CreateJob from './company/CreateJob';
@@ -27,15 +19,8 @@ const Body = () => {
         <Routes>
           <Route element={<ProtectedRoute />}>
             <Route element={<Dashboard />} path="/dashboard" exact />
-            <Route element={<Customer />} path="/customers" />
-            <Route element={<AddCustomer />} path="/add_customer" />
-            <Route element={<AddCustomer1 />} path="/add_customer1" />
-            <Route element={<AddCompany />} path="/add_company" />
-            <Route element={<AddVendor />} path="/add_vendor" />
+          
             <Route element={<CreateJob />} path="/create_job" />
-            <Route element={<EditCompany />} path="/edit_company/:id" />
-            <Route element={<Importcha />} path="/import_chart_of_account" />
-            <Route element={<AssetList />} path="/asset_chart_of_account" />
             <Route element={<JobsList />} path="/jobs_list" />
             <Route element={<JobDetails />} path="/job_details/:id" />
             <Route element={<JobsPosition />} path="/jobs_position" />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,46 +1,23 @@
-// export const BASE_URL = "http://127.0.0.1:8000/";
-// export const LOGIN_URL = "http://127.0.0.1:8000/api/login";
-// export const LOGOUT_URL = "http://127.0.0.1:8000/api/logout";
-// export const ALL_USER = "http://127.0.0.1:8000/api/users/get_all_users";
-// export const JOBS_LIST = "http://127.0.0.1:8000/api/jobs/get_jobs";
-// export const JOB_DETAILS = "http://127.0.0.1:8000/api/jobs/get_jobs_details/";
-// export const ATTRIBUTES_ALL = "http://127.0.0.1:8000/api/jobs/get_all";
-// export const CREATE_JOB = "http://127.0.0.1:8000/api/jobs/create_update";
+export const BASE_URL = "http://127.0.0.1:8000/";
+export const LOGIN_URL = "http://127.0.0.1:8000/api/login";
+export const LOGOUT_URL = "http://127.0.0.1:8000/api/logout";
+export const ALL_USER = "http://127.0.0.1:8000/api/users/get_all_users";
+export const JOBS_LIST = "http://127.0.0.1:8000/api/jobs/get_jobs";
+export const JOB_DETAILS = "http://127.0.0.1:8000/api/jobs/get_jobs_details/";
+export const ATTRIBUTES_ALL = "http://127.0.0.1:8000/api/jobs/get_all";
+export const CREATE_JOB = "http://127.0.0.1:8000/api/jobs/create_update";
 
+
+ 
 
 // export const BASE_URL = "https://thegiftwala.in/";
 // export const LOGIN_URL = "https://thegiftwala.in/api/login";
 // export const LOGOUT_URL = "https://thegiftwala.in/api/logout";
 // export const ALL_USER = "https://thegiftwala.in/api/users/get_all_users";
 // export const JOBS_LIST = "https://thegiftwala.in/api/jobs/get_jobs";
+// export const JOB_DETAILS = "https://thegiftwala.in/api/jobs/get_jobs_details/";
+// export const ATTRIBUTES_ALL = "https://thegiftwala.in/api/jobs/get_all";
+// export const CREATE_JOB = "https://thegiftwala.in/api/jobs/create_update";
+ 
 
-export const BASE_URL = "https://thegiftwala.in/";
-export const LOGIN_URL = "https://thegiftwala.in/api/login";
-export const LOGOUT_URL = "https://thegiftwala.in/api/logout";
-export const ALL_USER = "https://thegiftwala.in/api/users/get_all_users";
-export const JOBS_LIST = "https://thegiftwala.in/api/jobs/get_jobs";
-export const JOB_DETAILS = "https://thegiftwala.in/api/jobs/get_jobs_details/";
-export const ATTRIBUTES_ALL = "https://thegiftwala.in/api/jobs/get_all";
-export const CREATE_JOB = "https://thegiftwala.in/api/jobs/create_update";
-
-
-
-
-
-
-
-export const COMPANY_PROFILE =  "https://api.vadsconsulting.com/api/company/save_profile";
-export const EDIT_PROFILE = "https://api.vadsconsulting.com/api/company/edit_profile"; 
-export const SAVE_BILLING = "https://api.vadsconsulting.com/api/company/save_billing";
-export const SAVE_CONTACT = "https://api.vadsconsulting.com/api/company/save_contact_person";
-export const SAVE_ADDRESS = "https://api.vadsconsulting.com/api/company/store_address";
-export const CHARTS_OF_ACCOUNTS = "https://api.vadsconsulting.com/api/charts_of_accounts";
-export const COMPANY_DUPLICATE = "https://api.vadsconsulting.com/api/company/check_duplicate_company"; 
-export const COMPANY_DISPLAY = "https://api.vadsconsulting.com/api/company/check_display"; 
-export const COMPANY_PROFILE_DETAIL = "https://api.vadsconsulting.com/api/company/get_company_details";
-export const COMPANY_BILLING_DETAIL = "https://api.vadsconsulting.com/api/company/get_billing_details";
-export const COMPANY_EDIT_ADDRESS = "https://api.vadsconsulting.com/api/company/get_edit_address_details";
-export const COMPANY_EDIT_CONTACT = "https://api.vadsconsulting.com/api/company/get_contact_details";
-export const DELETE_CONTACT_API = "https://api.vadsconsulting.com/api/company/delete_contact";
-export const ADD_VENDOR = "https://api.vadsconsulting.com/api/vendors/add";
-
+ 
