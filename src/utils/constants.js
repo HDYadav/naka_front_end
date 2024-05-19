56739ba16c3dc1e88503ba77235b2cc6052a1a58
Hari Yadav <h.hariy2k@gmail.com>
2024-05-19 19:36:45 +0530

    url change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,25 @@
-export const BASE_URL = "http://127.0.0.1:8000/";
-export const LOGIN_URL = "http://127.0.0.1:8000/api/admin_login";
-export const LOGOUT_URL = "http://127.0.0.1:8000/api/logout";
-export const ALL_USER = "http://127.0.0.1:8000/api/users/get_all_users";
-export const JOBS_LIST = "http://127.0.0.1:8000/api/jobs/get_jobs";
-export const JOB_DETAILS = "http://127.0.0.1:8000/api/jobs/get_jobs_details/";
-export const ATTRIBUTES_ALL = "http://127.0.0.1:8000/api/jobs/get_all";
-export const CREATE_JOB = "http://127.0.0.1:8000/api/jobs/create_update";
+// export const BASE_URL = "http://127.0.0.1:8000/";
+// export const LOGIN_URL = "http://127.0.0.1:8000/api/admin_login";
+// export const LOGOUT_URL = "http://127.0.0.1:8000/api/logout";
+// export const ALL_USER = "http://127.0.0.1:8000/api/users/get_all_users";
+// export const JOBS_LIST = "http://127.0.0.1:8000/api/jobs/get_jobs";
+// export const JOB_DETAILS = "http://127.0.0.1:8000/api/jobs/get_jobs_details/";
+// export const ATTRIBUTES_ALL = "http://127.0.0.1:8000/api/jobs/get_all";
+// export const CREATE_JOB = "http://127.0.0.1:8000/api/jobs/create_update";
+// export const GET_COMPNAY = "http://127.0.0.1:8000/api/jobs/get_company";
 
-export const GET_COMPNAY = "http://127.0.0.1:8000/api/jobs/get_company";
+
+ 
+
+export const BASE_URL = "https://thegiftwala.in/";
+export const LOGIN_URL = "https://thegiftwala.in/api/admin_login";
+export const LOGOUT_URL = "https://thegiftwala.in/api/logout";
+export const ALL_USER = "https://thegiftwala.in/api/users/get_all_users";
+export const JOBS_LIST = "https://thegiftwala.in/api/jobs/get_jobs";
+export const JOB_DETAILS = "https://thegiftwala.in/api/jobs/get_jobs_details/";
+export const ATTRIBUTES_ALL = "https://thegiftwala.in/api/jobs/get_all";
+export const CREATE_JOB = "https://thegiftwala.in/api/jobs/create_update";
+export const GET_COMPNAY = "https://thegiftwala.in/api/jobs/get_company";
 
 
  
