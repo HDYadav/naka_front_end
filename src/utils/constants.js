be0ec2a46b428101bade7bc2927cd13b05110bda
Hari Yadav <h.hariy2k@gmail.com>
2024-05-19 19:34:17 +0530

    auth login admin

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL = "http://127.0.0.1:8000/";
-export const LOGIN_URL = "http://127.0.0.1:8000/api/login";
+export const LOGIN_URL = "http://127.0.0.1:8000/api/admin_login";
 export const LOGOUT_URL = "http://127.0.0.1:8000/api/logout";
 export const ALL_USER = "http://127.0.0.1:8000/api/users/get_all_users";
 export const JOBS_LIST = "http://127.0.0.1:8000/api/jobs/get_jobs";
@@ -11,15 +11,6 @@ export const GET_COMPNAY = "http://127.0.0.1:8000/api/jobs/get_company";
 
 
  
-
-// export const BASE_URL = "https://thegiftwala.in/";
-// export const LOGIN_URL = "https://thegiftwala.in/api/login";
-// export const LOGOUT_URL = "https://thegiftwala.in/api/logout";
-// export const ALL_USER = "https://thegiftwala.in/api/users/get_all_users";
-// export const JOBS_LIST = "https://thegiftwala.in/api/jobs/get_jobs";
-// export const JOB_DETAILS = "https://thegiftwala.in/api/jobs/get_jobs_details/";
-// export const ATTRIBUTES_ALL = "https://thegiftwala.in/api/jobs/get_all";
-// export const CREATE_JOB = "https://thegiftwala.in/api/jobs/create_update";
  
 
  
